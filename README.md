454fed3fdb18d47254fea9cf539e2ab0010593b1
Mikkel Ricky <rimi@aarhus.dk>
2018-08-22 14:41:08 +0200

    SALG-440: Added user management

--- README.md
+++ README.md
@@ -10,6 +10,14 @@ bin/console doctrine:database:create
 bin/console doctrine:migrations:migrate --no-interaction
 ```
 
+Create users
+
+```sh
+bin/console fos:user:create --super-admin super-admin super-admin@example.com super-admin-password
+bin/console fos:user:create admin admin@example.com admin-password
+bin/console fos:user:promote admin ROLE_ADMIN
+```
+
 Build assets
 
 ```sh
---
+++ src/Entity/User.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of Sorterings-ABC.
+ *
+ * (c) 2018 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
