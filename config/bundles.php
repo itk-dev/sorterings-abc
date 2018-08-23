427863d364328284db0b88bd59eb3fa910beb4af
Mikkel Ricky <rimi@aarhus.dk>
2018-08-23 11:36:24 +0200

    SALG-440: Added CKEditor to text fields

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
