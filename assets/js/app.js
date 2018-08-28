ff6b59a3530d566442b0bbd1a96a25511ff03bd1
Jesper Pedersen <jesper@yepz.dk>
2018-08-28 14:02:48 +0200

    SALG-460: IE11 fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,14 @@ import Vue from 'vue'
 import makeI18n from './i18n'
 import Items from './components/Items'
 const container = document.getElementById('sorteringsabc-app')
-const $ = require('jquery');
-require('bootstrap/js/dist/collapse');
+const $ = require('jquery')
+require('bootstrap/js/dist/collapse')
+
+// IE11 ES6 fix
+import Es6Promise from 'es6-promise'
+Es6Promise.polyfill()
+import "isomorphic-fetch"
+import "babel-polyfill"
 
 if (container !== null) {
     const config = JSON.parse(container.getAttribute('data-configuration'))
